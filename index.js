69e52c41733cbe5eed5c05f63b55b76f51d45272
Brian Solano <brian_solano110012@yahoo.com>
2024-11-05 00:47:56 -0600

    completed code

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
-import { writefile } from 'node:fs' 
+import fs from "fs"; 
+import generateMarkdown from './utils/generateMarkdown.js';
+
+
 
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer.prompt([
     {
-        type: 'input'
+        type: 'input',
+        name: 'title',        
+        message: 'What is name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'name',        
         message: 'What is your name?'
-
+    },
+    {
+        type: 'input',
+        name: 'Github',        
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',        
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'description',        
+        message: 'Please write a short description of your project',
+    },
+    {
+        type: 'list',
+        name: 'license',        
+        message: 'What kind of license should the project have?',
+        choices: ['MIT','Apache 2.0','GPL 3.0','None'],
     }
-];
+])
+
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+.then((answers) => {
+    const markdown = generateMarkdown(answers);
+
+    fs.writeFile('README.md', markdown, (err) =>
+    err ? console.log(err) : console.log('README.md created successfully!')
+);
+});
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'Apache 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+    case 'GPL 3.0':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'None':
+      return '';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'Apache 2.0':
+      return '[Apache 2.0 License](https://www.apache.org/licenses/LICENSE-2.0)';
+    case 'GPL 3.0':
+      return '[GPL 3.0 License](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'None':
+      return '';
+    default:
+      return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return `
+## License
+${renderLicenseBadge(license)}
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${license} license. For more information, see ${renderLicenseLink(license)}.
+`;
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, you can contact me at [${data.email}](mailto:${data.email}).
 `;
 }
 
 export default generateMarkdown;
+
+
+
